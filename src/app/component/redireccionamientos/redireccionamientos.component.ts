4b84bb82fae8c2ef0732cd5f96e22ef5c9b6d3dc
Alex Agurto <alexdaniel148@hotmail.com>
2025-05-16 13:51:57 -0500

    Avance Pagina Principal

--- src/app/component/redireccionamientos/redireccionamientos.component.ts
+++ src/app/component/redireccionamientos/redireccionamientos.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { PaginaprincipalComponent } from "../../paginaprincipal/paginaprincipal.component";
 import { MainComponent } from "../../paginaprincipal/main/main.component";
+import { CarrouselComponent } from '../../paginaprincipal/carrousel/carrousel.component';
+import { FooterComponent } from "../../paginaprincipal/footer/footer.component";
 
 @Component({
   selector: 'app-redireccionamientos',
   standalone: true,
-  imports: [PaginaprincipalComponent, MainComponent],
+  imports: [PaginaprincipalComponent, MainComponent, CarrouselComponent, FooterComponent],
   templateUrl: './redireccionamientos.component.html',
   styleUrl: './redireccionamientos.component.css'
 })
