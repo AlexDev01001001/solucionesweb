a5fcc972d4d02939d6b90c56fc35051dfefac92c
Alex Agurto <alexdaniel148@hotmail.com>
2025-05-16 10:58:34 -0500

    SolucionesWeb

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { Routes,RouterModule } from '@angular/router';
+import { MainComponent } from './paginaprincipal/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',redirectTo:'/home',pathMatch:'full'},
+    {path:'home',component:MainComponent}
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
