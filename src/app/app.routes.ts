9936a5f665390047b49a1d1f3bf63d4ce8e92a20
Alex Agurto <alexdaniel148@hotmail.com>
2025-05-21 23:21:30 -0500

    Login para doctores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes,RouterModule } from '@angular/router';
 import { RedireccionamientosComponent } from './component/redireccionamientos/redireccionamientos.component';
 import { MainComponent } from './login/main/main.component';
 import { PaginacitasComponent } from './citas/paginacitas/paginacitas.component';
-
+import { LogindoctorComponent } from './vistadoctor/logindoctor/logindoctor.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'/home',pathMatch:'full'},
     {path:'home',component:RedireccionamientosComponent},
     {path:'login',component:MainComponent},
-    {path:'citas',component:PaginacitasComponent}
+    {path:'citas',component:PaginacitasComponent},
+    {path:'doctor', component:LogindoctorComponent}
 ];
 
 export const AppRoutingModule = RouterModule.forRoot(routes);
---
+++ src/app/vistadoctor/logindoctor/logindoctor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logindoctor',
+  standalone: true,
+  imports: [],
+  templateUrl: './logindoctor.component.html',
+  styleUrl: './logindoctor.component.css'
+})
+export class LogindoctorComponent {
+
+}
